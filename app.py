ecb56cf17b5fc4a9ce3297680f35fa87abf9baed
teh42eem00 <tomasz.macalka@student.pk.edu.pl>
2023-05-13 00:47:15 +0200

    Added file preprocessing inside application, removed label bug, module separation

--- app.py
+++ app.py
@@ -3,15 +3,25 @@
 # For more details, please refer to the following papers:
 # [1] A. Joulin, E. Grave, P. Bojanowski, T. Mikolov, "Bag of Tricks for Efficient Text Classification," arXiv preprint arXiv:1607.01759 (2016).
 # [2] A. Joulin, E. Grave, P. Bojanowski, M. Douze, H. Jégou, T. Mikolov, "FastText.zip: Compressing text classification models," arXiv preprint arXiv:1612.03651 (2016).
-
+import os
 
 from flask import Flask, render_template, request
-import fasttext
+
+from language_detection import detect_language, train_own_model, load_176_model
+from file_utils import process_file
 
 app = Flask(__name__)
-own_model = fasttext.train_supervised(input='static/combined.txt', label_prefix="__label__", epoch=25, lr=0.1,
-                                      wordNgrams=1, bucket=2000000, dim=300, thread=2)
-model = fasttext.load_model('static/lid.176.ftz')
+
+combined_file_path = 'static/combined.txt'
+if os.path.exists(combined_file_path):
+    os.remove(combined_file_path)
+
+process_file('static/eng_sentences.tsv', 'en', 50000)
+process_file('static/pol_sentences.tsv', 'pl', 50000)
+process_file('static/deu_sentences.tsv', 'de', 50000)
+
+own_model = train_own_model()
+model_176 = load_176_model()
 
 
 @app.route('/')
@@ -22,24 +32,8 @@ def index():
 @app.route('/', methods=['POST'])
 def process_text():
     text = request.form['text']
-    results = detect_language(text)
+    results = detect_language(text, own_model, model_176)
     return render_template('index.html', text=text, results=results)
-
-
-def detect_language(text):
-    own_predictions = own_model.predict(text, k=2)
-    own_results = []
-    for label, probability in zip(own_predictions[0], own_predictions[1]):
-        language = label.replace('__label__', '')
-        own_results.append({'language': language, 'probability': probability})
-
-    pretrained_predictions = model.predict(text, k=2)
-    pretrained_results = []
-    for label, probability in zip(pretrained_predictions[0], pretrained_predictions[1]):
-        language = label.replace('__label__', '')
-        pretrained_results.append({'language': language, 'probability': probability})
-
-    return {'own_model': own_results, 'pretrained_model': pretrained_results}
 
 
 if __name__ == '__main__':
